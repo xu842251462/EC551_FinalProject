e9fa863edacc3edec51143b2c19dc157ed7e76b0
cnaught1 <98363115+cnaught1@users.noreply.github.com>
2022-10-04 15:10:36 -0400

    Adding ADC Development Folder
    Includes Digilent provided ADC demo for Basys3 dev board as well as spec sheet for FSR

---
+++ ADC Development/Demos/Basys3-Demo/hw.gen/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.v
@@ -0,0 +1,41 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+// Date        : Thu Aug 25 14:59:28 2022
+// Host        : sim-ro running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub d:/WorkBORI/Basys-3/hw/src/ip/xadc_wiz_0/xadc_wiz_0_stub.v
+// Design      : xadc_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module xadc_wiz_0(daddr_in, dclk_in, den_in, di_in, dwe_in, vauxp6, 
+  vauxn6, vauxp7, vauxn7, vauxp14, vauxn14, vauxp15, vauxn15, busy_out, channel_out, do_out, drdy_out, 
+  eoc_out, eos_out, alarm_out, vp_in, vn_in)
+/* synthesis syn_black_box black_box_pad_pin="daddr_in[6:0],dclk_in,den_in,di_in[15:0],dwe_in,vauxp6,vauxn6,vauxp7,vauxn7,vauxp14,vauxn14,vauxp15,vauxn15,busy_out,channel_out[4:0],do_out[15:0],drdy_out,eoc_out,eos_out,alarm_out,vp_in,vn_in" */;
+  input [6:0]daddr_in;
+  input dclk_in;
+  input den_in;
+  input [15:0]di_in;
+  input dwe_in;
+  input vauxp6;
+  input vauxn6;
+  input vauxp7;
+  input vauxn7;
+  input vauxp14;
+  input vauxn14;
+  input vauxp15;
+  input vauxn15;
+  output busy_out;
+  output [4:0]channel_out;
+  output [15:0]do_out;
+  output drdy_out;
+  output eoc_out;
+  output eos_out;
+  output alarm_out;
+  input vp_in;
+  input vn_in;
+endmodule
---
+++ ADC Development/Demos/Basys3-Demo/hw.gen/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.vhdl
@@ -0,0 +1,48 @@
+-- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+-- Date        : Thu Aug 25 14:59:28 2022
+-- Host        : sim-ro running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub d:/WorkBORI/Basys-3/hw/src/ip/xadc_wiz_0/xadc_wiz_0_stub.vhdl
+-- Design      : xadc_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity xadc_wiz_0 is
+  Port ( 
+    daddr_in : in STD_LOGIC_VECTOR ( 6 downto 0 );
+    dclk_in : in STD_LOGIC;
+    den_in : in STD_LOGIC;
+    di_in : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dwe_in : in STD_LOGIC;
+    vauxp6 : in STD_LOGIC;
+    vauxn6 : in STD_LOGIC;
+    vauxp7 : in STD_LOGIC;
+    vauxn7 : in STD_LOGIC;
+    vauxp14 : in STD_LOGIC;
+    vauxn14 : in STD_LOGIC;
+    vauxp15 : in STD_LOGIC;
+    vauxn15 : in STD_LOGIC;
+    busy_out : out STD_LOGIC;
+    channel_out : out STD_LOGIC_VECTOR ( 4 downto 0 );
+    do_out : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    drdy_out : out STD_LOGIC;
+    eoc_out : out STD_LOGIC;
+    eos_out : out STD_LOGIC;
+    alarm_out : out STD_LOGIC;
+    vp_in : in STD_LOGIC;
+    vn_in : in STD_LOGIC
+  );
+
+end xadc_wiz_0;
+
+architecture stub of xadc_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "daddr_in[6:0],dclk_in,den_in,di_in[15:0],dwe_in,vauxp6,vauxn6,vauxp7,vauxn7,vauxp14,vauxn14,vauxp15,vauxn15,busy_out,channel_out[4:0],do_out[15:0],drdy_out,eoc_out,eos_out,alarm_out,vp_in,vn_in";
+begin
+end;
